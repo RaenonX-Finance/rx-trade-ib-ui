007cbce8d1019f3849dadb273956cbfea9fc46c4
RaenonX <raenonx0710@gmail.com>
2022-02-20 00:31:22 -0600

    Added Px Side for executions
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/layout/pxData/tradeLog/body.tsx
+++ src/layout/pxData/tradeLog/body.tsx
@@ -37,11 +37,18 @@ export const TradeLogOffcanvas = ({executions, showNoPnL}: Props) => {
           <th>P&L</th>
           <th>P&L Sum</th>
           <th>Side</th>
+          <th>Qty</th>
+          <th>Px Side</th>
+          <th>Px Side Sum</th>
           <th>Profit</th>
           <th>Loss</th>
           <th>WR</th>
           <th>Avg Profit</th>
           <th>Avg Loss</th>
+          <th>R/R</th>
+          <th>EWR</th>
+          <th>Avg Px+</th>
+          <th>Avg Px-</th>
           <th>R/R</th>
           <th>EWR</th>
         </tr>
@@ -57,17 +64,36 @@ export const TradeLogOffcanvas = ({executions, showNoPnL}: Props) => {
               <td className={getClassName(execution.realizedPnL)}>
                 {execution.realizedPnL && formatSignedNumber(execution.realizedPnL, 2)}
               </td>
-              <td className={getClassName(execution.totalPnL)}>
-                {execution.totalPnL && formatSignedNumber(execution.totalPnL, 2)}
+              <td className={getClassName(execution.realizedPnLSum)}>
+                {execution.realizedPnLSum && formatSignedNumber(execution.realizedPnLSum, 2)}
               </td>
               <td style={{color: execSideColor[execution.side]}}>{execution.side}</td>
+              <td>{execution.quantity}</td>
+              <td className={getClassName(execution.pxSide)}>
+                {execution.pxSide && formatSignedNumber(execution.pxSide, 2)}
+              </td>
+              <td className={getClassName(execution.pxSideSum)}>
+                {execution.pxSideSum && formatSignedNumber(execution.pxSideSum, 2)}
+              </td>
               <td className={styles['up']}>{execution.realizedPnL && execution.profit}</td>
               <td className={styles['down']}>{execution.realizedPnL && execution.loss}</td>
               <td>{execution.realizedPnL && execution.winRate?.toFixed(3)}</td>
-              <td className={styles['up']}>{execution.realizedPnL && execution.avgTotalProfit?.toFixed(2)}</td>
-              <td className={styles['down']}>{execution.realizedPnL && execution.avgTotalLoss?.toFixed(2)}</td>
-              <td>{execution.realizedPnL && execution.avgTotalRrRatio?.toFixed(3)}</td>
-              <td>{execution.realizedPnL && execution.thresholdWinRate?.toFixed(3)}</td>
+              <td className={styles['up']}>
+                {execution.realizedPnL && execution.avgPnLProfit?.toFixed(2)}
+              </td>
+              <td className={styles['down']}>
+                {execution.realizedPnL && execution.avgPnLLoss?.toFixed(2)}
+              </td>
+              <td>{execution.realizedPnL && execution.avgPnLRrRatio?.toFixed(3)}</td>
+              <td>{execution.realizedPnL && execution.avgPnLEwr?.toFixed(3)}</td>
+              <td className={styles['up']}>
+                {execution.realizedPnL && execution.avgPxProfit?.toFixed(2)}
+              </td>
+              <td className={styles['down']}>
+                {execution.realizedPnL && execution.avgPxLoss?.toFixed(2)}
+              </td>
+              <td>{execution.realizedPnL && execution.avgPxRrRatio?.toFixed(3)}</td>
+              <td>{execution.realizedPnL && execution.avgPxEwr?.toFixed(3)}</td>
             </tr>
           ))}
       </tbody>
