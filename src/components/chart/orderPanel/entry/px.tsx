0d2ebb1ae3f5ed15bc7d72c799f29737ff3d1f24
RaenonX <raenonx0710@gmail.com>
2022-03-02 04:44:08 -0600

    Added Px diff in order entry
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/components/chart/orderPanel/entry/px.tsx
+++ src/components/chart/orderPanel/entry/px.tsx
@@ -1,31 +1,48 @@
 import React from 'react';
 
+import Col from 'react-bootstrap/Col';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
 
 import {forceMinTick} from '../../../../utils/calc';
+import {formatSignedNumber} from '../../../../utils/string';
 import {OrderPanelCommonProps} from '../type';
+import {StatsField} from './statsField';
 
 
 type Props = OrderPanelCommonProps & {
   pxTick: number,
 };
 
-export const OrderPanelPx = ({order, setOrder, pxTick}: Props) => {
+export const OrderPanelPx = ({order, setOrder, pxTick, position}: Props) => {
+  const {px} = order;
+  const {avgPx} = position;
+
   return (
-    <FloatingLabel label="Px" className="mb-3">
-      <Form.Control
-        size="lg"
-        type="number"
-        placeholder=""
-        className="text-end"
-        value={forceMinTick(order.px, pxTick)}
-        onChange={(e) => (
-          setOrder({px: forceMinTick(parseFloat(e.currentTarget.value), pxTick)})
-        )}
-        onMouseOver={(e) => e.currentTarget.focus()}
-        step={pxTick}
-      />
-    </FloatingLabel>
+    <Row className="g-3">
+      <Col xs={7}>
+        <FloatingLabel label="Px" className="mb-3">
+          <Form.Control
+            size="lg"
+            type="number"
+            placeholder=""
+            className="text-end"
+            value={forceMinTick(px, pxTick)}
+            onChange={(e) => (
+              setOrder({px: forceMinTick(parseFloat(e.currentTarget.value), pxTick)})
+            )}
+            onMouseOver={(e) => e.currentTarget.focus()}
+            step={pxTick}
+          />
+        </FloatingLabel>
+      </Col>
+      <Col xs={5}>
+        <StatsField
+          label="Px Diff"
+          value={formatSignedNumber(px - avgPx, 2)}
+        />
+      </Col>
+    </Row>
   );
 };
