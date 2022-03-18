22fe2ce8c31b6e6000dac73ed5a08b608b230428
RaenonX <raenonx0710@gmail.com>
2022-03-18 16:43:03 -0500

    Fixed modal / offcanvas z-index
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/layout/pxData/tradeLog/pnlTrend/main.tsx
+++ src/layout/pxData/tradeLog/pnlTrend/main.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Modal from 'react-bootstrap/Modal';
 
 import {isRealizedExecution, TradeLogProps} from '../type';
+import styles from './main.module.scss';
 import {PnLTrendPlot} from './plot';
 
 
@@ -15,7 +16,7 @@ export const PnLTrend = ({symbol, executions, show, setShow}: Props) => {
   const realizedExecutions = executions.filter(isRealizedExecution);
 
   return (
-    <Modal show={show} size="xl" centered onHide={() => setShow(false)}>
+    <Modal show={show} size="xl" centered onHide={() => setShow(false)} className={styles['pnl-trend-modal']}>
       <Modal.Header closeButton>
         <Modal.Title>{`PnL Trend (${symbol})`}</Modal.Title>
       </Modal.Header>
